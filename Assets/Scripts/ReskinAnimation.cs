1012e3366ff70d245a07ccf1e3115a0e96abcb6a
Robert Carag <carag.robert@gmail.com>
2021-06-24 18:26:44 +0800

    prototyping spritesheet reskinner

---
+++ Assets/Scripts/ReskinAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Based on https://youtu.be/rMCLWt1DuqI?t=1316
+    /// </summary>
+    
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ReskinAnimation : MonoBehaviour
+    {
+        public Texture2D ReskinTexture;
+        public List<Sprite> ReskinSprites;
+
+        // Set in Awake()
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            
+            foreach (var sprite in ReskinSprites)
+            {
+                _spritesDict[sprite.name] = sprite;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            var spriteName = _spriteRenderer.sprite.name;
+            
+            if (!_spritesDict.ContainsKey(spriteName))
+                return;
+
+            _spriteRenderer.sprite = _spritesDict[spriteName];
+        }
+
+        private void OnValidate()
+        {
+            if (ReskinTexture == null)
+                return;
+            
+            string spritesheetPath = AssetDatabase.GetAssetPath(ReskinTexture);
+            
+            ReskinSprites.Clear();
+
+            var sprites = AssetDatabase.LoadAllAssetsAtPath(spritesheetPath)
+                .OfType<Sprite>();
+
+            ReskinSprites.AddRange(sprites);    ;
+        }
+
+        private readonly Dictionary<string, Sprite> _spritesDict = new Dictionary<string, Sprite>();
+    }
+}
