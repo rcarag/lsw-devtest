f05cfeca276eff6901b054d49d877bcd54433551
Robert Carag <carag.robert@gmail.com>
2021-06-24 21:33:39 +0800

    added DialogSO

---
+++ Assets/Scripts/Dialog/DialogSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "Game/DialogSO")]
+    public class DialogSO : ScriptableObject
+    {
+        [Tooltip("Set to empty string to hide the name label")]
+        [SerializeField] private string _name = "John";
+        [SerializeField] private string _body = "Hello World";
+        
+        [Tooltip("Set to empty string to hide the positive button")]
+        [SerializeField] private string _positiveButtonLabel = "Yes";
+        [Tooltip("Set to empty string to hide the negative button")]
+        [SerializeField] private string _negativeButtonLabel = "No";
+
+        public string Name => _name;
+        public string Body => _body;
+        public string PositiveButtonLabel => _positiveButtonLabel;
+        public string NegativeButtonLabel => _negativeButtonLabel;
+        
+        public bool NameVisible => _name.Length > 0;
+        public bool PositiveButtonVisible => _positiveButtonLabel.Length > 0;
+        public bool NegativeButtonVisible => _negativeButtonLabel.Length > 0;
+    }
+}
