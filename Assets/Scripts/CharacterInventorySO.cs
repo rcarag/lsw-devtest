18e3687329edf6e9399ebc6983e3df261c1dd7a1
Robert Carag <carag.robert@gmail.com>
2021-06-25 01:19:11 +0800

    added CharacterInventorySO,  equip slots in ItemSO

---
+++ Assets/Scripts/CharacterInventorySO.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "Game/CharacterInventorySO")]
+    public class CharacterInventorySO : ScriptableObject
+    {
+        [Header("See tooltips for more info")]
+        [Tooltip("The equip slots that are available for this inventory")]
+        [SerializeField] private List<StringConstant> _equipSlots = new List<StringConstant>();
+        
+        [Tooltip("Drag ItemSOs to set as initial inventory contents here")]
+        [SerializeField] private List<ItemSO> _inventory = new List<ItemSO>();
+        
+        [Tooltip("Drag ItemSOs to set as initial equipped items here (must exist in inventory and be equippable)")]
+        [SerializeField] private List<ItemSO> _equippedItems = new List<ItemSO>();
+        
+        public IReadOnlyCollection<ItemSO> Items => _inventory.AsReadOnly();
+
+        private void OnValidate()
+        {
+            ValidateEquippedItems();
+        }
+
+        private void ValidateEquippedItems()
+        {
+            for (int i = _equippedItems.Count - 1; i >= 0; i--)
+            {
+                if (!EquippedItemIsValid(_equippedItems[i]))
+                {
+                    _equippedItems.RemoveAt(i);
+                }
+            }
+        }
+
+        private bool EquippedItemIsValid(ItemSO item)
+        {
+            if (item == null) return true;
+            
+            return HasItem(item) && CanEquipItem(item);
+        }
+
+        public bool HasItem(ItemSO item)
+        {
+            return _inventory.Contains(item);
+        }
+
+        public bool CanEquipItem(ItemSO item)
+        {
+            foreach (var requiredSlot in item.RequiredEquipSlots)
+            {
+                if (requiredSlot == null)
+                    continue;
+
+                if (!_equipSlots.Contains(requiredSlot))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Items/ItemSO.cs
+++ Assets/Scripts/Items/ItemSO.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 namespace Game
@@ -7,14 +8,16 @@ namespace Game
     [CreateAssetMenu(menuName = "Game/ItemSO")]
     public class ItemSO : ScriptableObject
     {
-        [SerializeField] private string _itemName;
-        [SerializeField] private Sprite _icon;
-        [SerializeField] private int _buyPrice;
-        [SerializeField] private int _sellPrice;
+        [SerializeField] private string _itemName = "Shirt";
+        [SerializeField] private Sprite _icon = null;
+        [SerializeField] private int _buyPrice = 1;
+        [SerializeField] private int _sellPrice = 1;
+        [SerializeField] private List<StringConstant> _requiredEquipSlots = new List<StringConstant>();
 
         public string ItemName => _itemName;
         public Sprite Icon => _icon;
         public int BuyPrice => _buyPrice;
         public int SellPrice => _sellPrice;
+        public IReadOnlyCollection<StringConstant> RequiredEquipSlots => _requiredEquipSlots.AsReadOnly();
     }
 }
