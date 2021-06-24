48095e336950f89a99dd67d13e41d0ba84691228
Robert Carag <carag.robert@gmail.com>
2021-06-25 00:12:05 +0800

    added ItemSO

---
+++ Assets/Scripts/Items/ItemSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "Game/ItemSO")]
+    public class ItemSO : ScriptableObject
+    {
+        [SerializeField] private string _itemName;
+        [SerializeField] private Sprite _icon;
+        [SerializeField] private int _buyPrice;
+        [SerializeField] private int _sellPrice;
+
+        public string ItemName => _itemName;
+        public Sprite Icon => _icon;
+        public int BuyPrice => _buyPrice;
+        public int SellPrice => _sellPrice;
+    }
+}
