824819427b256dc79d89d6b162ae095aedcf0b89
Robert Carag <carag.robert@gmail.com>
2021-06-25 16:18:00 +0800

    populating Shop screen

---
+++ Assets/Scripts/UI/Elements/ShopTransactionButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    [RequireComponent(typeof(Button))]
+    public class ShopTransactionButton : MonoBehaviour
+    {
+        [Header("UI Elements")]
+        [SerializeField] private Image _iconImage = null;
+        [SerializeField] private Text _priceText = null;
+
+        // Set in Initialize()
+        private ItemSO _item;
+        private TransactionType _transactionType;
+        
+        public void Initialize(ItemSO item, TransactionType transactionType)
+        {
+            _item = item;
+            _transactionType = transactionType;
+            
+            _iconImage.sprite = item.Icon;
+            _priceText.text = "$" + GetPrice();
+        }
+
+        private int GetPrice()
+        {
+            if (_transactionType == TransactionType.Buy)
+                return _item.BuyPrice;
+
+            if (_transactionType == TransactionType.Sell)
+                return _item.SellPrice;
+
+            throw new NotImplementedException();
+        }
+
+        public enum TransactionType
+        {
+            Buy,
+            Sell
+        }
+    }
+}
--- Assets/Scripts/UI/ShopScreen.cs
+++ Assets/Scripts/UI/ShopScreen.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,18 +12,65 @@ namespace Game
     {
         [Header("UI Elements")]
         [SerializeField] private Button _closeButton = null;
+        [SerializeField] private RectTransform _playerInventoryItemsGrid;
+        [SerializeField] private RectTransform _shopInventoryItemsGrid;
 
         [Header("Events")]
+        [SerializeField] private VoidEvent _shopScreenOpenedEvent = null;
         [SerializeField] private VoidEvent _shopScreenClosedEvent = null;
+
+        [Header("Factories")]
+        [SerializeField] private PrefabFactorySO _shopBuyButtonFactory = null;
+        [SerializeField] private PrefabFactorySO _shopSellButtonFactory = null;
+        
+        [Header("Model")]
+        [SerializeField] private CharacterInventorySO _playerInventory = null;
+        [SerializeField] private CharacterInventorySO _shopInventory = null;
         
         private void Awake()
         {
+            _shopScreenOpenedEvent.Register(OnShopScreenOpened);
+            
             _closeButton.onClick.AddListener(OnCloseButtonPressed);
+        }
+
+        private void OnDestroy()
+        {
+            _shopScreenOpenedEvent.Unregister(OnShopScreenOpened);
         }
 
         private void OnCloseButtonPressed()
         {
             _shopScreenClosedEvent.Raise();
+        }
+
+        private void OnShopScreenOpened()
+        {
+            InitializeShopItems();
+        }
+
+        private void InitializeShopItems()
+        {
+            foreach (var item in _playerInventory.Items)
+            {
+                if (_playerInventory.EquippedItems.Contains(item))
+                    continue;
+
+                _shopSellButtonFactory.Create(_playerInventoryItemsGrid, delegate(GameObject instance)
+                {
+                    var txButton = instance.GetComponent<ShopTransactionButton>();
+                    txButton.Initialize(item, ShopTransactionButton.TransactionType.Sell);
+                });
+            }
+            
+            foreach (var item in _shopInventory.Items)
+            {
+                _shopBuyButtonFactory.Create(_shopInventoryItemsGrid, delegate(GameObject instance)
+                {
+                    var txButton = instance.GetComponent<ShopTransactionButton>();
+                    txButton.Initialize(item, ShopTransactionButton.TransactionType.Buy);
+                });
+            }
         }
     }
 
